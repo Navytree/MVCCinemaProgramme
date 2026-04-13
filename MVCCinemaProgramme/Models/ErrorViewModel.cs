8ffac35179a6ef1f6d6314c2f284d3394a8c6a56
Navytree <olglanis@gmail.com>
2026-04-13 17:51:18 +0200

    Initial commit - new cinema programme project

---
+++ MVCCinemaProgramme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCinemaProgramme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
